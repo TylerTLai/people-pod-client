5ca8b812a8edd7eaf122e5cb805a67eca861a70f
Tyler Lai <lai.t.tyler@gmail.com>
2020-11-29 17:13:23 -0600

    Add Header to Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import Header from '../../sections/header/Header';
 import * as Styles from './LayoutStyles';
 
 function Layout({ children }) {
   return (
     <Styles.StyledContainer>
-      <div className="header">header</div>
+      <div className="header"><Header /></div>
       <div className="main">{children}</div>
       <div className="sidebar">sidebar</div>
       <div className="personInfo">person info</div>
