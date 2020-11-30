17de881eb19094fec54cdfaff03cdbef07afab5a
Tyler Lai <lai.t.tyler@gmail.com>
2020-11-30 06:57:00 -0600

    Update PersonItem and PeopleList

--- src/components/PeopleList/PeopleList.js
+++ src/components/PeopleList/PeopleList.js
@@ -1,22 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { connect } from 'react-redux';
 
+import { getPeople } from '../../store/actions/personActions';
 import PersonItem from '../PersonItem/PersonItem';
 import * as Styles from './PeopleListStyles';
 
-function PeopleList({ setModalInfo }) {
-  const [people, setPeople] = useState(['a', 'b', 'c', 'd', 'e']);
+function PeopleList({
+  getPeople,
+  group,
+  people,
+  person,
+  searchTerm,
+  setModalInfo,
+}) {
+  const getPeopleBasedOnGroup = useCallback(() => {
+    if (group === null || group === undefined) {
+      return getPeople();
+    } else if (group === 'favorite') {
+      return getPeople('favorite');
+    } else {
+      return getPeople(group._id);
+    }
+  }, [group, people, person]);
 
-  const ppl = people.map((person) => {
-    return <PersonItem name={person} setModalInfo={setModalInfo} />;
+  useEffect(() => {
+    getPeopleBasedOnGroup();
+  }, [group, person]);
+
+  // console.log('from peoplelist ', people);
+
+  const filteredPeople = people.filter((person) => {
+    return person.fName.indexOf(searchTerm.toLowerCase()) !== -1;
   });
 
-  return <Styles.StyledContainer>{ppl}</Styles.StyledContainer>;
+  if (filteredPeople.length > 0) {
+    const listOfPeople = filteredPeople.map((person) => {
+      return (
+        <PersonItem
+          person={person}
+          key={person._id}
+          setModalInfo={setModalInfo}
+        />
+      );
+    });
 
-  //   return (
-  //     <Styles.StyledContainer>
-  //       <p>No people in the pod. Click "Add New Person" to add a person.</p>
-  //     </Styles.StyledContainer>
-  //   );
+    return <Styles.StyledContainer>{listOfPeople}</Styles.StyledContainer>;
+  } else {
+    return (
+      <Styles.StyledContainer>
+        <p>No people in the pod. Click "Add New Person" to add a person.</p>
+      </Styles.StyledContainer>
+    );
+  }
 }
 
-export default PeopleList;
+const mapStateToProps = (state) => {
+  const { searchTerm, people, person } = state.people;
+
+  return {
+    person,
+    people,
+    group: state.group.group,
+    searchTerm,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getPeople: (groupId) => dispatch(getPeople(groupId)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PeopleList);
--- src/components/PersonItem/PersonItem.js
+++ src/components/PersonItem/PersonItem.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
 import { Heart, Edit, Trash } from 'react-feather';
 
 import * as Styles from './PersonItemStyles';
+import { deletePerson, getPerson } from '../../store/actions/personActions';
 import { StyledButton } from '../../styles/Button/Button';
+import { PERSON_ERROR } from '../../store/actions/personTypes';
 
-function PersonItem({ setModalInfo }) {
-  // const showForm = (person) => {
-  //   setModalInfo((prevState) => ({
-  //     show: !prevState.show,
-  //     modal: 'AddPerson',
-  //     person,
-  //   }));
-  // };
+function PersonItem({ deletePerson, getPerson, person, setModalInfo }) {
+  useEffect(() => {
+    const alreadyFaved = person.group.some((el) => el.groupName === 'Favorite');
+    setFave(alreadyFaved);
+  }, [person]);
+
+  const [fave, setFave] = useState(false);
+
+  const favoritePerson = (personId) => {
+    axios
+      .put('/api/people/favorite/' + personId)
+      .then((res) => setFave(res.data.favorited));
+  };
+
+  const showForm = (person) => {
+    setModalInfo((prevState) => ({
+      show: !prevState.show,
+      modal: 'AddPerson',
+      person,
+    }));
+  };
 
   return (
-    <Styles.StyledLink>
+    <Styles.StyledLink to={'/preson/' + person._id}>
       <Styles.StyledContainer>
         <Styles.StyledContentArea>
           <div>
             <Styles.StyledPic src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/002irm_ons_crd_03.jpg" />
           </div>
           <div>
-            <Styles.StyledName>Name</Styles.StyledName>
+            <Styles.StyledName>{`${person.fName} ${person.lName}`}</Styles.StyledName>
             <Styles.StyledLocation>Location</Styles.StyledLocation>
-            <Styles.StyledNote>Note</Styles.StyledNote>
+            <Styles.StyledNote>
+              {person.note.length >= 130
+                ? person.note.slice(0, 130) + '...'
+                : person.note}
+            </Styles.StyledNote>
           </div>
         </Styles.StyledContentArea>
         <Styles.StyledButtonsArea>
@@ -35,7 +56,20 @@ function PersonItem({ setModalInfo }) {
             border="none"
             background="none"
           >
-            <Heart size={16} style={Styles.featherIconHeartFilledStyles} />
+            {fave ? (
+              <Heart
+                size={16}
+                style={Styles.featherIconHeartFilledStyles}
+                onClick={() => favoritePerson(person._id)}
+              />
+            ) : (
+              <Heart
+                size={16}
+                style={Styles.featherIconHeartNotFilledStyles}
+                onClick={() => favoritePerson(person._id)}
+              />
+            )}
+
             <Styles.StyledButtonText>Favorite</Styles.StyledButtonText>
           </StyledButton>
           <StyledButton
@@ -45,6 +79,7 @@ function PersonItem({ setModalInfo }) {
             height="30px"
             border="none"
             background="none"
+            onClick={() => showForm(person)}
           >
             <Edit size={16} style={Styles.featherIconEditStyles} />
             <Styles.StyledButtonText>Edit</Styles.StyledButtonText>
@@ -56,6 +91,7 @@ function PersonItem({ setModalInfo }) {
             height="30px"
             border="none"
             background="none"
+            onClick={() => deletePerson(person._id)}
           >
             <Trash size={16} style={Styles.featherIconDeleteStyles} />
             <Styles.StyledButtonText>Delete</Styles.StyledButtonText>
@@ -66,4 +102,11 @@ function PersonItem({ setModalInfo }) {
   );
 }
 
-export default PersonItem;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getPerson: (personId) => dispatch(getPerson(personId)),
+    deletePerson: (personId) => dispatch(deletePerson(personId)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(PersonItem);
