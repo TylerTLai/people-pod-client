3deaea0e7cba17af634f927a92b98ac54de1c7b8
Tyler Lai <lai.t.tyler@gmail.com>
2020-11-29 20:18:15 -0600

    Add Modal to App and update Modal

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from '../../pages/Home';
+import Modal from '../Modal/Modal';
 
 function App() {
+  const [modalInfo, setModalInfo] = useState({
+    show: false,
+    modal: '',
+    person: null,
+  });
+
   return (
     <div className="App">
+      <Modal modalInfo={modalInfo} setModalInfo={setModalInfo} />
       <Switch>
         <Route exact path="/">
           <Home />
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,25 +3,27 @@ import React from 'react';
 import { backdropVariants, modalVariants } from './ModalAnimations';
 import * as Styles from './ModalStyles';
 
-function Modal() {
+function Modal({ modalInfo, setModalInfo }) {
   return (
     <Styles.StyledAnimatePresence exitBeforeEnter>
-      <Styles.StyledBackdrop
-        variants={backdropVariants}
-        initial="hidden"
-        animate="visible"
-        exit="hidden"
-      >
-        <Styles.StyledContainer
-          variants={modalVariants}
-          //   animate={
-          //     showModal.modal === 'AddPerson' ? 'visiblePerson' : 'visibleGroup'
-          //   }
-          exit="exit"
+      {modalInfo.show === true && (
+        <Styles.StyledBackdrop
+          variants={backdropVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
         >
-          Add Person Form
-        </Styles.StyledContainer>
-      </Styles.StyledBackdrop>
+          <Styles.StyledContainer
+            variants={modalVariants}
+            //   animate={
+            //     modalInfo.modal === 'AddPerson' ? 'visiblePerson' : 'visibleGroup'
+            //   }
+            exit="exit"
+          >
+            Add Person Form
+          </Styles.StyledContainer>
+        </Styles.StyledBackdrop>
+      )}
     </Styles.StyledAnimatePresence>
   );
 }
