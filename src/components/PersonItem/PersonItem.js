f154e02d9d6865d025b8f3a7fc75f09e75994f84
Tyler Lai <lai.t.tyler@gmail.com>
2020-11-29 18:18:17 -0600

    Add content and styles to PersonItem

--- src/components/PersonItem/PersonItem.js
+++ src/components/PersonItem/PersonItem.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from 'react';
+import FeatherIcon from 'feather-icons-react';
+
+import * as Styles from './PersonItemStyles';
+import { StyledButton } from '../../styles/Button/Button';
 
 function PersonItem() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <Styles.StyledLink>
+      <Styles.StyledContainer>
+        <Styles.StyledContentArea>
+          <div>
+            <Styles.StyledPic src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/002irm_ons_crd_03.jpg" />
+          </div>
+          <div>
+            <Styles.StyledName>Name</Styles.StyledName>
+            <Styles.StyledLocation>Location</Styles.StyledLocation>
+            <Styles.StyledNote>Note</Styles.StyledNote>
+          </div>
+        </Styles.StyledContentArea>
+        <Styles.StyledButtonsArea>
+          <StyledButton
+            margin=".2rem"
+            padding="0 .2rem"
+            width="80px"
+            height="30px"
+            border="none"
+            background="none"
+          >
+            favorite
+          </StyledButton>
+          <StyledButton
+            margin=".2rem"
+            padding="0 .2rem"
+            width="80px"
+            height="30px"
+            border="none"
+            background="none"
+          >
+            edit
+          </StyledButton>
+          <StyledButton
+            margin=".2rem"
+            padding="0 .2rem"
+            width="80px"
+            height="30px"
+            border="none"
+            background="none"
+          >
+            delete
+          </StyledButton>
+        </Styles.StyledButtonsArea>
+      </Styles.StyledContainer>
+    </Styles.StyledLink>
+  );
 }
 
-export default PersonItem
+export default PersonItem;
---
+++ src/components/PersonItem/PersonItemStyles.js
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+import theme from '../../styles/theme';
+
+const { colors } = theme;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+export const StyledContainer = styled(motion.div)`
+  display: grid;
+  grid-template-columns: auto 2fr;
+  grid-template-rows: auto 50px;
+  align-items: center;
+  border-radius: 5px;
+  background: ${colors.white};
+  border: 1px solid ${colors.offWhite2};
+  padding: 1rem 2rem 0.5rem 2rem;
+  grid-template-areas:
+    'contentArea contentArea'
+    'buttonsArea buttonsArea';
+`;
+
+export const StyledPic = styled.img`
+  margin: 0 auto;
+  border-radius: 5px;
+  width: 100px;
+  height: auto;
+`;
+
+export const StyledContentArea = styled.div`
+  grid-area: contentArea;
+  align-items: flex-start;
+  display: grid;
+  grid-template-columns: 132px auto;
+`;
+
+export const StyledButtonsArea = styled.div`
+  grid-area: buttonsArea;
+  display: grid;
+  grid-template-columns: 110px 1fr auto;
+  justify-items: left;
+  align-self: flex-end;
+`;
+
+export const StyledName = styled.h2`
+  margin: 0;
+`;
+
+export const StyledLocation = styled.p`
+  margin-top: 7px;
+  color: ${colors.spunPearl};
+`;
+
+export const StyledNotes = styled.p`
+  color: ${colors.gray};
+`;
