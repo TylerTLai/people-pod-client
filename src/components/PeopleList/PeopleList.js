b730fe57fded9fa19d65ef90ea68d436978b174e
Tyler Lai <lai.t.tyler@gmail.com>
2020-11-29 19:02:17 -0600

    Add dummy data to PeopleList

--- src/components/PeopleList/PeopleList.js
+++ src/components/PeopleList/PeopleList.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import PersonItem from '../PersonItem/PersonItem';
 import * as Styles from './PeopleListStyles';
 
 function PeopleList() {
-  return (
-    <Styles.StyledContainer>
-      <p>No people in the pod. Click "Add New Person" to add a person.</p>
-    </Styles.StyledContainer>
-  );
+  const [people, setPeople] = useState(['a', 'b', 'c', 'd', 'e']);
+
+  const ppl = people.map((person) => {
+    return <PersonItem name={person} />;
+  });
+
+  return <Styles.StyledContainer>{ppl}</Styles.StyledContainer>;
+
+  //   return (
+  //     <Styles.StyledContainer>
+  //       <p>No people in the pod. Click "Add New Person" to add a person.</p>
+  //     </Styles.StyledContainer>
+  //   );
 }
 
 export default PeopleList;
