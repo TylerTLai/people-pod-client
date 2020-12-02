1eff98f65f3e96a0435dde352245db7a658357bc
Tyler Lai <lai.t.tyler@gmail.com>
2020-12-02 02:22:52 -0600

    Update PersonInfo

--- src/sections/personInfo/PersonInfo.js
+++ src/sections/personInfo/PersonInfo.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function PersonInfo() {
   const handleClick = () => {
-    axios.get('/api/people').then((res) => console.log('using axios ', res.data));
   };
 
   return (
