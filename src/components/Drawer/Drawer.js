30f81e81b81f36b48828e9ba90bb20583a0713a0
Tyler Lai <lai.t.tyler@gmail.com>
2020-12-02 02:19:07 -0600

    Update Drawer

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -48,7 +48,7 @@ function Drawer({ getGroups, groups, getGroup, getPeople, setModalInfo }) {
     <Styles.StyledContainer>
       <Styles.StyledGroupHeader>
         <div>
-          <Styles.StyledGroupTitle>Groups</Styles.StyledGroupTitle>
+          <Styles.StyledGroupTitle>Pods</Styles.StyledGroupTitle>
         </div>
         <div>
           <PlusCircle
